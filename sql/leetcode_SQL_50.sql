907def09cb68b5a94a3f15d1649fdb3b68532ad4
Aashish Tangnami <42236161+AashishTangnami@users.noreply.github.com>
2024-10-06 23:18:26 -0400

    medium diff q 7

--- sql/leetcode_SQL_50.sql
+++ sql/leetcode_SQL_50.sql
@@ -305,7 +305,6 @@ HAVING COUNT(e2.managerId) >= 5
 /*
 Question NO: 6
 Difficulty: Medium
-
 Table: Signups
 
 +----------------+----------+
@@ -425,3 +424,84 @@ FROM Signups AS s
 LEFT JOIN Confirmations AS c
 ON s.user_id = c.user_id
 GROUP BY s.user_id
+
+
+/*
+Question No: 7
+Difficulty: Medium
+
+Table: Transactions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table.
+The table has information about incoming transactions.
+The state column is an enum of type ["approved", "declined"].
+ 
+
+Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Transactions table:
++------+---------+----------+--------+------------+
+| id   | country | state    | amount | trans_date |
++------+---------+----------+--------+------------+
+| 121  | US      | approved | 1000   | 2018-12-18 |
+| 122  | US      | declined | 2000   | 2018-12-19 |
+| 123  | US      | approved | 2000   | 2019-01-01 |
+| 124  | DE      | approved | 2000   | 2019-01-07 |
++------+---------+----------+--------+------------+
+Output: 
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+| 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+| 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+
+select to_char(trans_date, 'YYYY-MM') as month, country, count(state) trans_count, count(case when state = 'approved' then state end) as approved_count, sum (amount) as trans_total_amount ,coalesce(sum(case when state = 'approved' then amount end),0) as approved_total_amount
+from Transactions
+group by month, country
+
+select to_char(trans_date,'yyyy-mm') as month,country, count(*) trans_count, 
+    count(case when state = 'approved' then 1 end) as  approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state = 'approved' then amount else 0 end) as  approved_total_amount
+from Transactions
+group by month, country
+*/
+-- select
+--     substring(trans_date::varchar, 1,7) as month,
+--     country,
+--     count(id) as trans_count,
+--     sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+--     sum(amount) as trans_total_amount,
+--     sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+-- from
+--     Transactions
+-- group by country, month
+
+SELECT TO_CHAR(trans_date, 'YYYY-MM') "month",
+       country,
+       COUNT(amount) trans_count,
+       COUNT(amount) FILTER ( WHERE state = 'approved') approved_count,
+       SUM(amount) trans_total_amount,
+       COALESCE(SUM(amount) FILTER ( WHERE state = 'approved' ), 0) approved_total_amount
+FROM Transactions
+GROUP BY country, month
