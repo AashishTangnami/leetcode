b8cedcd5da3c67258812b23657edd9e18a6a0158
Aashish Tangnami <42236161+AashishTangnami@users.noreply.github.com>
2024-10-29 17:48:18 -0400

    new advanced select join 1q sln

--- sql_50_leetcode/2_Basic_Joins/students_and_examinations.sql
+++ sql_50_leetcode/2_Basic_Joins/students_and_examinations.sql
@@ -112,7 +112,9 @@ SELECT s.student_id,
         COUNT(e.student_id) AS attended_exams
 FROM Students AS s
 CROSS JOIN Subjects AS sub
-LEFT JOIN Examinations AS e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+LEFT JOIN Examinations AS e 
+ON 
+        s.student_id = e.student_id AND sub.subject_name = e.subject_name
 GROUP BY s.student_id, 
             s.student_name, 
             sub.subject_name
